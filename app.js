c6a432c5e2b3700e485f4d383b1d7bca271cf7da
Samarth Bhardwaj <samarthb5109@gmail.com>
2021-09-07 00:26:49 +0530

    calling mock server

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
 //outputDiv.innerText = "translated text";
 //console.log(outputDiv);
+
+function getTranslationalURL(text){
+    return serverURL + "?" + "text=" + text;
+
+}
 
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
     //console.log("clicked");
     //console.log(txtInput.value);
-    outputDiv.innerText="translated text: " + txtInput.value;
-   
+    //outputDiv.innerText="translated text: " + txtInput.value;
+
+    var inputText = txtInput.value; // taking input
+
+    fetch(getTranslationalURL(inputText)) //calling server for processing
+    .then(response=>response.json())
+    .then(json=>console.log(json.contents.translated))
+  
 });
+
