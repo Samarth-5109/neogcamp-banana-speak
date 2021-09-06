ecf4274762d1f0995be947afd110cb1ca0050164
Samarth Bhardwaj <samarthb5109@gmail.com>
2021-09-06 17:40:37 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //for selecting button
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var Output = document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
+
+//outputDiv.innerText = "translated text";
+//console.log(outputDiv);
+
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
     //console.log("clicked");
-    console.log(txtInput.value);
-    console.log(Output.innerHTML="translated text");
+    //console.log(txtInput.value);
+    outputDiv.innerText="translated text: " + txtInput.value;
+   
 });
